558d40638a340b0d6cb2420c897f6537cff1104f
arancksj22 <arancksj@gmail.com>
2025-10-18 22:08:34 +0530

    controller get mapping

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,4 +1,16 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+
+    @GetMapping(/{num1}/{num2})
+    public String add(@PathVariable int num1, @PathVariable int num2) {
+        int sum = num1 + num2;
+        return "The sum of " + num1 + " and " + num2 + " is " + sum;
+    }
+
 }
